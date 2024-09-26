51c1e9cd506f9010342f4f5e3ef70b6d614aa96e
Nick Phelps <nicholas.phelps@proton.me>
2024-09-26 14:31:53 -0600

    Creating webserver and a few test APIs

---
+++ backend/data/data.js
@@ -0,0 +1,34 @@
+const chats = [
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Luke Skywalker",
+        email: "scruffyNerfHerder@lightside.com",
+      },
+      {
+        name: "Han Solo",
+        email: "chewiesBoy@smuggler.net",
+      },
+    ],
+    _id: "1a2b3c4d5e6f7g8h9i",
+    chatName: "Luke Skywalker",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Chewie",
+        email: "wookie4life@smuggler.net",
+      },
+      {
+        name: "Han Solo",
+        email: "chewiesBoy@smuggler.net",
+      },
+    ],
+    _id: "123456789abcedef",
+    chatName: "Han Solo",
+  },
+];
+
+module.exports = chats;
